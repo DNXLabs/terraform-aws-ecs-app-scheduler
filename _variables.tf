b7b8e8fbf50e11dd8ef3e44f274073082e1e0806
Lucas de Macedo <122332519+lucasmacedot@users.noreply.github.com>
2023-06-08 23:47:02 -0300

    Fix indent and add default value to some variables

--- _variables.tf
+++ _variables.tf
@@ -1,56 +1,59 @@
 variable "name" {
   description = "Name of your ECS service"
+  type        = string
 }
 
 variable "memory" {
   default     = "512"
   description = "Hard memory of the container"
+  type        = string
 }
 
 variable "cpu" {
   default     = "0"
   description = "Hard limit for CPU for the container"
+  type        = string
 }
 
 variable "cluster_name" {
   default = "Name of existing ECS Cluster to deploy this app to"
-  type = string
+  type    = string
 }
 
 variable "service_role_arn" {
   description = "Existing service role ARN created by ECS cluster module"
-  type = string
+  type        = string
 }
 
 variable "task_role_arn" {
   description = "Existing task role ARN created by ECS cluster module"
-  type = string
+  type        = string
 }
 
 variable "image" {
   description = "Docker image to deploy (can be a placeholder)"
   default     = "alpine:latest"
-  type = string
+  type        = string
 }
 
 variable "vpc_id" {
   description = "VPC ID to deploy this app to"
-  type = string
+  type        = string
 }
 
 variable "subnets_ids" {
   type    = list(string)
-  default = null
+  default = []
 }
 
 variable "security_group_ids" {
   type    = list(string)
-  default = null
+  default = []
 }
 
 variable "assign_public_ip" {
   type    = bool
-  default = null
+  default = false
 }
 
 variable "platform_version" {
@@ -60,25 +63,25 @@ variable "platform_version" {
 
 variable "cloudwatch_logs_retention" {
   default     = 120
-  type = number
+  type        = number
   description = "Specifies the number of days you want to retain log events in the specified log group. Possible values are: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, and 3653."
 }
 
 variable "cloudwatch_logs_export" {
   default     = false
-  type = bool
+  type        = bool
   description = "Whether to mark the log group to export to an S3 bucket (needs terraform-aws-log-exporter to be deployed in the account/region)"
 }
 
 variable "rule_enabled" {
   default     = true
-  type = bool
+  type        = bool
   description = "Whether the rule should be enabled"
 }
 
 variable "launch_type" {
   default     = "EC2"
-  type = string
+  type        = string
   description = "Instance type. Fargate allowed"
 }
 
@@ -94,7 +97,7 @@ variable "iam_path" {
 
 variable "event_description" {
   default = "ECS task"
-  type = string
+  type    = string
 }
 
 variable "account_id" {
