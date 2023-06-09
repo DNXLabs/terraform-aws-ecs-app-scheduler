20161653d2bb91c80d3fb13db56067737c96571b
Lucas de Macedo <122332519+lucasmacedot@users.noreply.github.com>
2023-06-08 23:31:53 -0300

    Add types to variables

--- _variables.tf
+++ _variables.tf
@@ -14,29 +14,28 @@ variable "cpu" {
 
 variable "cluster_name" {
   default = "Name of existing ECS Cluster to deploy this app to"
+  type = string
 }
 
 variable "service_role_arn" {
   description = "Existing service role ARN created by ECS cluster module"
+  type = string
 }
 
 variable "task_role_arn" {
   description = "Existing task role ARN created by ECS cluster module"
+  type = string
 }
 
 variable "image" {
   description = "Docker image to deploy (can be a placeholder)"
   default     = "alpine:latest"
+  type = string
 }
 
 variable "vpc_id" {
   description = "VPC ID to deploy this app to"
-}
-
-variable "launch_type" {
-  type    = string
-  default = "EC2"
-  description = "Valid values include: EC2, EXTERNAL, or FARGATE."
+  type = string
 }
 
 variable "subnets_ids" {
@@ -56,34 +55,41 @@ variable "assign_public_ip" {
 
 variable "cloudwatch_logs_retention" {
   default     = 120
+  type = number
   description = "Specifies the number of days you want to retain log events in the specified log group. Possible values are: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, and 3653."
 }
 
 variable "cloudwatch_logs_export" {
   default     = false
+  type = bool
   description = "Whether to mark the log group to export to an S3 bucket (needs terraform-aws-log-exporter to be deployed in the account/region)"
 }
 
 variable "rule_enabled" {
   default     = true
+  type = bool
   description = "Whether the rule should be enabled"
 }
 
 variable "launch_type" {
   default     = "EC2"
+  type = string
   description = "Instance type. Fargate allowed"
 }
 
 variable "schedule_expression" {
   description = "Cron expression"
+  type = string
 }
 
 variable "iam_path" {
   default = "/"
+  type = string
 }
 
 variable "event_description" {
   default = "ECS task"
+  type = string
 }
 
 variable "account_id" {
